32f677ea192bd17c1309208450adf2bdb3279218
acruzr <acruzr135@gmail.com>
2020-05-23 09:21:11 -0500

    refactor node dls 3

--- jenkins/dsl/nodejs.groovy
+++ jenkins/dsl/nodejs.groovy
@@ -1,8 +1,9 @@
 job('NodeJS example') {
 	scm {
-		git('git@github.com:acruzr/service-web-app.git') { node -> // is hundson.plugins.git.GitSCM
+		git('git@github.com:acruzr/service-web-app.git', 'ssh') { node -> // is hundson.plugins.git.GitSCM
 			node / gitConfigName('DSL user')
 			node / gitConfigEmail('acruzr135@gmail.com')
+			credentials('ssh_jenkins_ID')
 		}
 	}
 	wrappers {
